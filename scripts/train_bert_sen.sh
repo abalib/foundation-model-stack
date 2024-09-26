65bacc6a8a348ad4e02f8dfea67cfe144c8e6538
Bulent Abali <abali@us.ibm.com>
2024-09-26 19:16:40 +0000

    less verbose training

--- scripts/train_bert_sen.sh
+++ scripts/train_bert_sen.sh
@@ -9,24 +9,24 @@ export HOST_ENDIAN_FORMAT=big
 
 export SENCORELETS=2
 export SENCORES=32
-#export DEE_DUMP_GRAPHS=granite8b
+# export DEE_DUMP_GRAPHS=granite8b
 export TOKENIZERS_PARALLELISM=false
-#export DATA_PREC=fp16
+# export DATA_PREC=fp16
 export SENLIB_DEVEL_CONFIG_FILE=/root/.senlib.json
 export AIU_CONFIG_FILE_0=/root/.senlib.json
-#export FLEX_RDMA_PCI_BUS_ADDR_0="0000:00:00.0"
+# export FLEX_RDMA_PCI_BUS_ADDR_0="0000:00:00.0"
 export AIU_WORLD_RANK_0="0000:00:00.0"
 export DTLOG_LEVEL=error
-export TORCH_SENDNN_LOG=DEBUG
-###unset TORCH_SENDNN_LOG
+### export TORCH_SENDNN_LOG=DEBUG
+### unset TORCH_SENDNN_LOG
 export DT_DEEPRT_VERBOSE=-1
-#export COMPILATION_MODE=offline_decoder
-#export DTCOMPILER_KEEP_EXPORT=true
+# export COMPILATION_MODE=offline_decoder
+# export DTCOMPILER_KEEP_EXPORT=true
 export FLEX_COMPUTE=SENTIENT
 export FLEX_DEVICE=VFIO
-#export FLEX_OVERWRITE_NMB_FRAME=1
-#export FLEX_MONITOR_SLEEP=0
-#unset FMS_SKIP_TP_EMBEDDING
+# export FLEX_OVERWRITE_NMB_FRAME=1
+# export FLEX_MONITOR_SLEEP=0
+# unset FMS_SKIP_TP_EMBEDDING
 
 
 MODEL_PATH=/apps/bert-base-uncased
